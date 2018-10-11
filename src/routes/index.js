9eb16c66d26cea63c0376764b5f7c126f8c5562e
Renan Miyamoto <renan.miyamoto@okn.com.br>
2018-10-11 14:16:42 -0300

    build

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ import {BrowserRouter, Switch, Route} from "react-router-dom";
 import {publicRoutes} from "./routes";
 
 const Routes = () => (
-	<BrowserRouter>
+	<BrowserRouter basename="/movies/build">
 		<Switch>
 			{publicRoutes.map(route => (
 				<Route key={route.path} {...route} exact />
